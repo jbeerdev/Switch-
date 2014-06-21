1b1b6a4016480f2b9af0a0dfba844361f0a169a1
Jose Molino <jose@l332.home>
2014-06-21 19:11:16 +0200

    Menu and main screen

--- src/es/task/switcher/Main.java
+++ src/es/task/switcher/Main.java
@@ -3,8 +3,13 @@ package es.task.switcher;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 
-public class Main extends Activity {
+public class Main extends Activity implements OnMenuItemClickListener {
     /** Called when the activity is first created. */
    
 	@Override
@@ -12,4 +17,30 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	
+	
+	public void showPopup(View v) {
+	    PopupMenu popup = new PopupMenu(this, v);
+	    MenuInflater inflater = popup.getMenuInflater();
+	    popup.setOnMenuItemClickListener(this);
+
+	    inflater.inflate(R.menu.main_menu, popup.getMenu());
+	    popup.show();
+	}
+	
+
+
+	@Override
+	public boolean onMenuItemClick(MenuItem item) {
+		  switch (item.getItemId()) {
+	        case R.id.menu_archive:
+	            
+	            return true;
+	        case R.id.menu_delete:
+	           
+	            return true;
+	        default:
+	            return false;
+	    }
+	}
 }
